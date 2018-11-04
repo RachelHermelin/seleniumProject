1550801679aa477ac336c275d69243ec64936c80
RachelHermelin <44698292+RachelHermelin@users.noreply.github.com>
2018-11-04 13:01:49 +0200

    Delete extraConstants.java

--- src/extraConstants.java
+++
@@ -1,7 +0,0 @@
-import org.openqa.selenium.By;
-
-public class extraConstants {
-    public static final By enterButton=(By.className("db "));
-    public static final By errorText=(By.xpath("//li[@='כל המתנות מחכות לך! אבל קודם צריך מייל וסיסמה']"));
-
-}
